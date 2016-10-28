17bdcfe7b1e859998d82e81ac10dd85eb99f2644
muryoimpl <k-muryoi@esm.co.jp>
2016-10-28 22:34:28 +0900

    Add ruby script which creates symlinks to $HOME
    symlink を作成する ruby script を追加した

---
+++ symlink.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+
+IGNORE_FILES = %w(. .. .DS_Store .git)
+
+current_dir = Dir.pwd
+
+dotfiles = Dir.glob('.*').select {|f| !IGNORE_FILES.include?(f) }
+
+dotfiles.each do |dotfile|
+  FileUtils.ln_s("#{current_dir}/#{dotfile}", "#{ENV['HOME']}/#{dotfile}", force: true)
+  # puts "#{current_dir}/#{dotfile}  ->  #{ENV['HOME']}/#{dotfile}"
+end
