d9815200a47c687777ba75fc702a15455431237f
muryoimpl <muryoimpl@gmail.com>
2017-08-26 03:24:05 +0900

    Enable titlebars_enabled in rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -621,8 +621,8 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      -- }, properties = { titlebars_enabled = true }
-      }, properties = { titlebars_enabled = false }
+      }, properties = { titlebars_enabled = true }
+      -- }, properties = { titlebars_enabled = false }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
