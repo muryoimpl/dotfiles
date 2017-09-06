b2414b9a613c08521a7e71ee975bf65361014642
muryoimpl <muryoimpl@gmail.com>
2017-09-07 05:01:09 +0900

    Use expand path of .rbenv and .nodenv

--- install.d/install_env.sh
+++ install.d/install_env.sh
@@ -1,16 +1,16 @@
 # rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/rbenv-default-gems
-echo "bundler" > $(rbenv root)/default-gems
-git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/rbenv/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+echo "bundler" > ~/.rbenv/plugins/default-gems
+git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
 
 # nodenv
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-mkdir -p "$(nodenv root)"/plugins
-git clone https://github.com/nodenv/nodenv-update.git "$(nodenv root)"/plugins/nodenv-update
-git clone https://github.com/nodenv/node-build.git $(nodenv root)/plugins/node-build
+mkdir -p ~/.nodenv/plugins
+git clone https://github.com/nodenv/nodenv-update.git ~/.nodenv/plugins/nodenv-update
+git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
 
 # goenv
 git clone https://github.com/syndbg/goenv.git ~/.goenv
