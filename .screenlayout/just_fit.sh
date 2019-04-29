a113d4be135bd298cb72bc11178d6d23dbcd038a
muryoimpl <k-muryoi@esm.co.jp>
2019-04-29 16:06:42 +0900

    Use raw depth

--- .screenlayout/just_fit.sh
+++ .screenlayout/just_fit.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 2048x1152 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+# xrandr --output VIRTUAL1 --off --output eDP1 --mode 2048x1152 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
