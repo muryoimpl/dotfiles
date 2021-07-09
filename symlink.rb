a86ce96d3303fb23046e761311bd68fa52718551
muryoimpl <muryoimpl@gmail.com>
2021-07-09 14:59:26 +0900

    symlink.rb の IGNORE_FILES を更新

--- symlink.rb
+++ symlink.rb
@@ -1,12 +1,12 @@
 require 'fileutils'
 
-IGNORE_FILES = %w(. .. .DS_Store .git .config)
+IGNORE_FILES = %w(. .. .DS_Store .git .config install.d)
 
 current_dir = Dir.pwd
 
-dotfiles = Dir.glob('.*').select {|f| !IGNORE_FILES.include?(f) }
+dotfiles = Dir.glob('.*').reject {|f| IGNORE_FILES.include?(f) }
 
 dotfiles.each do |dotfile|
-  FileUtils.ln_s("#{current_dir}/#{dotfile}", "#{ENV['HOME']}/#{dotfile}", force: true)
-  # puts "#{current_dir}/#{dotfile}  ->  #{ENV['HOME']}/#{dotfile}"
+  # FileUtils.ln_s("#{current_dir}/#{dotfile}", "#{ENV['HOME']}/#{dotfile}", force: true)
+  puts "#{current_dir}/#{dotfile}  ->  #{ENV['HOME']}/#{dotfile}"
 end
