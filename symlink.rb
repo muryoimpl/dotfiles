f14d94e1a7a8d03e7e82eb0a514da06476df8bdd
muryoimpl <muryoimpl@gmail.com>
2023-04-30 11:39:35 +0900

    symlink.rb: 既存のsymlinkを再作成するように変更する

--- symlink.rb
+++ symlink.rb
@@ -17,6 +17,8 @@ current_dir = Dir.pwd
 dotfiles = Dir.glob('.*').reject {|f| ignored.include?(f) }
 
 dotfiles.each do |dotfile|
+  file_in_home = "#{ENV['HOME']}/#{dotfile}"
+  FileUtils.safe_unlink(file_in_home) if FileTest.symlink?(file_in_home)
   FileUtils.ln_s("#{current_dir}/#{dotfile}", "#{ENV['HOME']}/#{dotfile}", force: true)
   puts "#{current_dir}/#{dotfile}  ->  #{ENV['HOME']}/#{dotfile}"
 end
