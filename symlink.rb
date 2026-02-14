0a09ff7c645b1f89b2c6caaf71d8fd7b0ee3a3f5
muryoimpl <muryoimpl@gmail.com>
2026-02-15 00:51:28 +0900

    symlink.rb: .claude の中身をリンクする処理を入れた(未確認

--- symlink.rb
+++ symlink.rb
@@ -17,6 +17,8 @@ current_dir = Dir.pwd
 dotfiles = Dir.glob('.*').reject {|f| ignored.include?(f) }
 dotfiles << 'local'
 
+claude_paths = Dir.glob('_claude/*')
+
 FileUtils.cd ENV['HOME']
 
 dotfiles.each do |dotfile|
@@ -27,4 +29,14 @@ dotfiles.each do |dotfile|
   puts "#{current_dir}/#{dotfile}  ->  #{ENV['HOME']}/#{dotfile}"
 rescue
   puts "Error occurred. #{dotfile}, file_in_home: #{file_in_home}"
+end
+
+claude_paths.each do |path|
+  path_in_claude = ".#{path[1..-1]}"
+  FileUtils.safe_unlink(path_in_claude) if FileTest.symlink?(path_in_claude)
+
+  FileUtils.ln_s("#{RELATIVE_PATH}/#{path}", "./#{path_in_claude}", force: true)
+  puts "#{current_dir}/#{path}  ->  #{ENV['HOME']}/#{path_in_claude}"
+rescue
+  puts "Error occurred. #{path}, path_in_claude: #{path_in_claude}"
 end
